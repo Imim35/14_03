1cbf7a08f38da444a116b57dbe6e1e50731e2de7
Evgeniy <geniaimim35@gmail.com>
2021-02-28 19:28:04 +0300

    wip form

--- SRO/pages/index.vue
+++ SRO/pages/index.vue
@@ -110,32 +110,37 @@
             </transition>
 
             <transition name="fade">
-              <el-form-item v-show="step === 6" prop="contacts" label="Предпочитаемый способ получения результатов расчета:">
+              <el-form-item v-show="step === 6" prop="contacts">
+                <div class="text-muted">Предпочитаемый способ получения результатов расчета:</div>
                 <el-checkbox-group v-model="form.contacts">
 
-                  <el-form-item prop="mobile">
+                  <el-form-item prop="mobile" class="checkbox-group">
                     <el-row>
-                      <el-checkbox label="Номер телефона" name="mobile">
+                      <el-checkbox label="mobile" name="mobile">
                         <label>Номер телефона:</label>
-                        <el-input size="small" placeholder="+7917" type="contacts" v-model="form.mobile"/>
+                        <el-input size="small" placeholder="+7917" type="number" v-model="form.mobile"/>
                       </el-checkbox>
                     </el-row>
                   </el-form-item>
 
+                  <el-form-item prop="email" class="checkbox-group">
+                    <el-row>
+                      <el-checkbox label="email" name="email">
+                        <label>Email:</label>
+                        <el-input size="small" placeholder="test@mail.ru" type="email" v-model="form.email" />
+                      </el-checkbox>
+                    </el-row>
+                  </el-form-item>
 
-                  <el-row>
-                    <el-checkbox label="Email" name="contacts">
-                      <label>Email:</label>
-                      <el-input size="small" placeholder="test@mail.ru" type="text" v-model="form.email" />
-                    </el-checkbox>
-                  </el-row>
+                  <el-form-item prop="whatsApp" class="checkbox-group">
+                    <el-row>
+                      <el-checkbox label="whatsApp" name="whatsApp">
+                        <label>WhatsApp:</label>
+                        <el-input size="small" placeholder="+7917" type="number" v-model="form.whatsApp"/>
+                      </el-checkbox>
+                    </el-row>
+                  </el-form-item>
 
-                  <el-row>
-                    <el-checkbox label="WhatsApp" name="contacts">
-                      <label>WhatsApp:</label>
-                      <el-input size="small" placeholder="+7917" type="number" v-model="form.whatsApp"/>
-                    </el-checkbox>
-                  </el-row>
 
                 </el-checkbox-group>
 
@@ -147,45 +152,6 @@
                 </div>
               </el-form-item>
             </transition>
-
-<!--            <transition name="fade">-->
-<!--              <el-form-item v-show="step === 6" prop="contacts" label="Предпочитаемый способ получения результатов расчета:">-->
-<!--                <div class="radio_items">-->
-<!--&lt;!&ndash;                  <div class="text-muted">Предпочитаемый способ получения результатов расчета:</div>&ndash;&gt;-->
-
-<!--                  <el-form-item prop="mobile">-->
-
-<!--                    <div class="contacts">-->
-<!--                      <el-checkbox v-model="mobile">Позвонить на номер:</el-checkbox>-->
-<!--                      <el-input v-show="mobile" size="small" placeholder="+7917" type="number" v-model="form.mobile"/>-->
-<!--                    </div>-->
-
-<!--                  </el-form-item>-->
-
-<!--                  <el-form-item prop="email">-->
-<!--                    <div class="contacts">-->
-<!--                      <el-checkbox v-model="email">Отправить сообщение на почту:</el-checkbox>-->
-<!--                      <el-input v-show="email" size="small" placeholder="test@mail.ru" type="email" v-model="form.email" />-->
-<!--                    </div>-->
-<!--                  </el-form-item>-->
-
-<!--                  <el-form-item prop="whatsApp">-->
-<!--                    <div class="contacts">-->
-<!--                      <el-checkbox v-model="whatsApp">Отправить сообщение на WhatsApp:</el-checkbox>-->
-<!--                      <el-input v-show="whatsApp" size="small" placeholder="+7917" type="number" v-model="form.whatsApp"/>-->
-<!--                    </div>-->
-<!--                  </el-form-item>-->
-<!--                  -->
-<!--                  <div style="display: flex;justify-content: space-between">-->
-<!--                    <div><el-button  size="mini" @click="backStep" plain>Назад</el-button></div>-->
-<!--                    <div>-->
-<!--                      <el-button  size="mini" @click="submit('form')" v-show="form.registrationLegalEntity && form.typeSRO && form.tender && form.conditionsWork && form.HPC && form.price && (mobile || email || whatsApp) " plain>Жду рассчета</el-button>-->
-<!--                    </div>-->
-<!--                  </div>-->
-<!--                </div>-->
-<!--              </el-form-item>-->
-<!--            </transition>-->
-
           </el-form>
         </el-collapse-item>
       </el-collapse>
@@ -210,24 +176,56 @@
 export default {
   data () {
     const validateMobile = (rule, value, callback) => {
-      console.log('rule',rule)
-      console.log('value', value)
-      console.log('callback', callback)
-      if (value === '') {
-        callback(new Error('Длина номера должна быть 6/11 цифр'));
-      } else {
-        if (this.form.mobile !== '') {
-          this.$refs.form.validateField('checkPass');
+      if (this.form.contacts.filter((b) => { return b.includes('mobile') }).length) {
+        if (value.length < 6) {
+          callback(new Error('Длина номера должна быть 6/12 цифр'));
+          this.$refs.form.validateField('form');
+        } else {
+          if (value.length > 12) {
+            callback(new Error('Длина номера должна быть 6/12 цифр'));
+            this.$refs.form.validateField('form');
+          }
+          callback();
         }
+      } else {
         callback();
       }
+    }
+    const validateEmail = (rule, value, callback) => {
+        if (this.form.contacts.filter((b) => { return b.includes('email') }).length) {
+          if (value.length < 6) {
+            callback(new Error('Длина номера должна быть 6/12 цифр'));
+            this.$refs.form.validateField('form');
+          } else {
+            if (value.length > 12) {
+              callback(new Error('Длина номера должна быть 6/12 цифр'));
+              this.$refs.form.validateField('form');
+            }
+            callback();
+          }
+        } else {
+          callback();
+        }
+    }
+    const validateWhatsApp = (rule, value, callback) => {
+        if (this.form.contacts.filter((b) => { return b.includes('whatsApp') }).length) {
+          if (value.length < 6) {
+            callback(new Error('Длина номера должна быть 6/12 цифр'));
+            this.$refs.form.validateField('form');
+          } else {
+            if (value.length > 12) {
+              callback(new Error('Длина номера должна быть 6/12 цифр'));
+              this.$refs.form.validateField('form');
+            }
+            callback();
+          }
+        } else {
+          callback();
+        }
     }
     return {
       step: 0,
       collapseShow: false,
-      // mobile      : true,
-      email       : false,
-      whatsApp    : false,
       form: {
         registrationLegalEntity: null, // Субьект регистрации Вашего юридического лица
         typeSRO                : null, // Тип СРО
@@ -235,28 +233,29 @@ export default {
         conditionsWork         : null, // Условия проведения работ
         HPC                    : null, // Имеются ли у вас специалисты, внесенные в HPC
         price                  : null, // Сумма одного договора
-        contacts               : ['Номер телефона'],
-        mobile                 : null, // Позвонить на номер
-        email                  : null, // Позвонить на номер
-        whatsApp               : null, // Позвонить на номер
+        contacts               : ['mobile'],
+        mobile                 : '', // Позвонить на номер
+        email                  : '', // EMAIL
+        whatsApp               : '', // Позвонить на whatsApp
       },
       rules: {
         contacts: [
           { type: 'array', required: true, message: 'Пожалуйста выберите один из вариантов', trigger: 'change' }
         ],
         mobile:[
-          { validator: validateMobile, trigger: 'blur' }
+          { validator: validateMobile, trigger:'blur' }
         ],
-        // email:[
-        //   { required: true, min: 3, max: 5, trigger: 'blur' }
-        // ],
-        // whatsApp:[
-        //   { required: true, min: 3, max: 5, trigger: 'blur' }
-        // ]
+        email:[
+          { validator: validateEmail, trigger: 'blur' }
+        ],
+        whatsApp:[
+          { validator: validateWhatsApp, trigger: 'blur' }
+        ]
       }
     }
   },
   methods: {
+
     nextStep () {
       if (this.step <= 5) {
         this.step += 1
@@ -322,6 +321,9 @@ export default {
       color: #a0a0a0;
       font-weight: bold;
       margin-bottom: 10px;
+    }
+    .checkbox-group {
+      margin-bottom: 15px;
     }
     .fade-enter,
     .fade-leave-to { opacity: 0 }
